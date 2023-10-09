c7540f7c3e2e1c264502ae06a23cf6079e5cac73
BjornRodin <bjornrodin@gmail.com>
2023-10-09 14:14:30 +0000

    Add products.html and views and urls

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Flowers
 
-# Create your views here.
+
+def all_products(request):
+    """ Showing all flowers, and will include sorting and search queries later """
+
+    products = Flowers.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
