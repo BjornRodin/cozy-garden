5a03abf435a81b21bd28c7cf4d05498fbff3c3d2
BjornRodin <bjornrodin@gmail.com>
2023-10-10 17:38:22 +0000

    Add shopping cart preview template and css to style

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Flowers
 
@@ -11,13 +11,14 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add items to the cart """
 
-    product = Flowers.objects.get(pk=item_id)
+    product = get_object_or_404(Flowers, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}.')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to your cart.')
@@ -29,13 +30,16 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """ Adjust the quantity of selected product """
 
+    product = get_object_or_404(Flowers, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}.')
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your cart.')
     
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -45,11 +49,12 @@ def remove_from_bag(request, item_id):
     """ Remove the selected product from the shopping cart """
 
     try:
-
+        product = get_object_or_404(Flowers, pk=item_id)
         bag = request.session.get('bag', {})
         bag.pop(item_id)
-        
+        messages.success(request, f'Removed {product.name} from your cart.')
         request.session['bag'] = bag
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'Error removing the item: {e}')
         return HttpResponse(status=500)
