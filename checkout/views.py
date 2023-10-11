5a349536b2682d09ddf6bfbae86f7d1020151cf1
BjornRodin <bjornrodin@gmail.com>
2023-10-11 12:51:52 +0000

    added stripe and it's elements and styles

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Nx6ZLEwXoLWvCjTQ2NY0lqan93Wj7UE1sYwUttGJIG4lzhs1dccUh4sbVGaVFAiEkcLG4iMsqfQP7sNcGEh0DOP009dpqJqmX',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
