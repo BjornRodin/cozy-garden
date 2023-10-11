cf44b8e64d6e147a35e0cb2051356d088791d4b7
BjornRodin <bjornrodin@gmail.com>
2023-10-11 18:52:37 +0000

    completed webhook handler

--- cozy_garden/settings.py
+++ cozy_garden/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5zvbg&&35dx3qn^%6p*q%zh31x7z=nzzy3m5tb4k=@t^1e)fw+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-bjornrodin-cozygarden-ilswcbi2jzz.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['localhost', '8000-bjornrodin-cozygarden-ilswcbi2jzz.ws-eu105.gitpod.io']
 
 
 # Application definition
