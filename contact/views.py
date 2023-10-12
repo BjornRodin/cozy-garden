dc96f8309819432fea38cc06654a2477c11f89ca
BjornRodin <bjornrodin@gmail.com>
2023-10-12 20:10:18 +0000

    Connected contact in urls and views and migrated model

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def contact_us(request):
+    """ Renders the contact page """
+    return render(request, 'contact/contact.html')
