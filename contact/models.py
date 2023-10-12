3c2cc9eb95153243f7323a0d2c778fdb56641628
BjornRodin <bjornrodin@gmail.com>
2023-10-12 19:53:31 +0000

    Add model for contact and urls in project

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactForm(models.Model):
+    """ Model for contact form """
+    class Meta:
+        verbose_name = 'Contact'
+
+    CATEGORY_CHOICES = [
+        ('General Question', 'General Question'),
+        ('Stock Suggestion', 'Stock Suggestion'),
+        ('Feedback', 'Feedback'),
+        ('Other', 'Other')
+    ]
+
+    category = models.CharField(max_length=30, choices=CATEGORY_CHOICES, default='General Question', null=False, blank=False)
+    name = models.CharField(max_length=80, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    subject = models.CharField(max_length=50, null=False, blank=False)
+    message = models.TextField(max_length=1000, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
