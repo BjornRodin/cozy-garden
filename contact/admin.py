2b36bdc5710f197ddbd7667496bafa44c3793123
BjornRodin <bjornrodin@gmail.com>
2023-10-13 09:28:05 +0000

    Updated contact admin

--- contact/admin.py
+++ contact/admin.py
@@ -2,27 +2,21 @@ from django.contrib import admin
 from .models import Contact
 
 
-admin.site.register(Contact)
-
-#class ContactFormAdmin(admin.ModelAdmin):
+class ContactFormAdmin(admin.ModelAdmin):
     
-#    list_display = [
-#        'name',
-#        'email',
-#       'subject',
-#        'created_at'
-#    ]
-#
-#    search_fields = [
-#        'name',
-#        'email',
-#        'subject'
-#    ]
-#
-#    list_filter = [
-#        'category',
-#        'created_at'
-#    ]
-#
-#
-#admin.site.register(ContactModel, ContactFormAdmin)
+    list_display = [
+        'email',
+        'subject',
+        'message',
+        'created_at',
+    ]
+
+    search_fields = [
+        'email',
+        'created_at',
+    ]
+
+    list_filter = ('email', 'created_at')
+
+
+admin.site.register(Contact, ContactFormAdmin)
