be6f1c300f672de17df4de8c9ad48facb93596c3
BjornRodin <bjornrodin@gmail.com>
2023-10-15 08:52:48 +0000

    debug set to true

--- cozy_garden/settings.py
+++ cozy_garden/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['cozy-gardenbrm-e89d3ea43ad4.herokuapp.com',
                  '8000-bjornrodin-cozygarden-ilswcbi2jzz.ws-eu105.gitpod.io']
