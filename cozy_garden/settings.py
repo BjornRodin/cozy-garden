eb129de5c0d61a6677645c1ec969bcc3b39e5ef4
BjornRodin <bjornrodin@gmail.com>
2023-10-15 00:56:29 +0000

    Debug = True

--- cozy_garden/settings.py
+++ cozy_garden/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+#DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['cozy-gardenbrm-e89d3ea43ad4.herokuapp.com',
                  '8000-bjornrodin-cozygarden-ilswcbi2jzz.ws-eu105.gitpod.io']
